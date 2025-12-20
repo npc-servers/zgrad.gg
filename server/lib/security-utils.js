76abc7418cfb603a8ee74a512dffe9c17c93c1b7
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2025-12-20 19:22:51 +0100

    Remove X-Frame-Options

--- server/lib/security-utils.js
+++ server/lib/security-utils.js
@@ -10,7 +10,6 @@ import { query } from './database.js';
  */
 export const SECURITY_HEADERS = {
   'X-Content-Type-Options': 'nosniff',
-  'X-Frame-Options': 'DENY',
   'X-XSS-Protection': '1; mode=block',
   'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
   'Referrer-Policy': 'strict-origin-when-cross-origin',
--- server/server.js
+++ server/server.js
@@ -55,7 +55,6 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.set({
     'X-Content-Type-Options': 'nosniff',
-    'X-Frame-Options': 'DENY',
     'X-XSS-Protection': '1; mode=block',
     'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
     'Referrer-Policy': 'strict-origin-when-cross-origin',
